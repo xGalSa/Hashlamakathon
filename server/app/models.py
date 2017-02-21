f5c653d00030d63cdaa859b373a6ef962d8b3334
Omer Horev <omer.hv@gmail.com>
2017-02-21 22:07:45 +0200

    Db working, starting reviews

--- server/app/models.py
+++ server/app/models.py
@@ -12,7 +12,30 @@ class UserSerializer(serializers.ModelSerializer):
 
 class SoldierUser(models.Model):
     armyID = models.CharField(max_length=8, primary_key=True)
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return "%s's soldier profile" % self.user
+
+class Driver(models.Model):
+    soldier = models.OneToOneField(SoldierUser, on_delete=models.CASCADE)
+    license_number = models.CharField(max_length=9)
+    expiration = models.DateField()
+
+class Vehicle(models.Model):
+    vehicle_number = models.CharField(max_length=10, primary_key=True)
+    vehicle_type = models.CharField(max_length=256)
+    current_drive = models.OneToOneField('Drive', blank=True, null=True )
+    # TODO: Add owners and privileges
+
+class Drive(models.Model):
+    driverID = models.ForeignKey(SoldierUser)
+    vehicle_number = models.ForeignKey(Vehicle)
+    source = models.CharField(max_length=256)
+    destination = models.CharField(max_length=256)
+    start_mileage = models.CharField(max_length=50)
+    end_mileage = models.CharField(max_length=50)
+    start_time = models.CharField(max_length=10)
+    end_time = models.CharField(max_length=10)
+    # add officer approval
+
